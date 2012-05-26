d1c776a78ed63870c0c8ae00d36a81685ec8f994
Alex Hopkins <arhopkins3@gmail.com>
2012-05-26 23:27:31 +0100

    Control visibility changes
    Controls now hidding during splitting process.

--- GlemnetCSVSplitter/Form1.Designer.cs
+++ GlemnetCSVSplitter/Form1.Designer.cs
@@ -42,13 +42,15 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.btnSplit = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblPleaseWait = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitAfter)).BeginInit();
             this.SuspendLayout();
             // 
             // lblSplitAfter
             // 
             this.lblSplitAfter.AutoSize = true;
-            this.lblSplitAfter.Location = new System.Drawing.Point(9, 182);
+            this.lblSplitAfter.Location = new System.Drawing.Point(12, 129);
             this.lblSplitAfter.Name = "lblSplitAfter";
             this.lblSplitAfter.Size = new System.Drawing.Size(122, 13);
             this.lblSplitAfter.TabIndex = 3;
@@ -56,7 +58,7 @@
             // 
             // splitAfter
             // 
-            this.splitAfter.Location = new System.Drawing.Point(12, 198);
+            this.splitAfter.Location = new System.Drawing.Point(15, 145);
             this.splitAfter.Maximum = new decimal(new int[] {
             1000000,
             0,
@@ -74,7 +76,7 @@
             // 
             // txtFileName
             // 
-            this.txtFileName.Location = new System.Drawing.Point(12, 71);
+            this.txtFileName.Location = new System.Drawing.Point(98, 43);
             this.txtFileName.Name = "txtFileName";
             this.txtFileName.Size = new System.Drawing.Size(252, 20);
             this.txtFileName.TabIndex = 4;
@@ -82,9 +84,9 @@
             // 
             // btnSelectFile
             // 
-            this.btnSelectFile.Location = new System.Drawing.Point(270, 69);
+            this.btnSelectFile.Location = new System.Drawing.Point(356, 41);
             this.btnSelectFile.Name = "btnSelectFile";
-            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.Size = new System.Drawing.Size(109, 23);
             this.btnSelectFile.TabIndex = 5;
             this.btnSelectFile.Text = "Select File";
             this.btnSelectFile.UseVisualStyleBackColor = true;
@@ -93,7 +95,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 55);
+            this.label1.Location = new System.Drawing.Point(9, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 6;
@@ -101,7 +103,7 @@
             // 
             // txtOptionText
             // 
-            this.txtOptionText.Location = new System.Drawing.Point(12, 132);
+            this.txtOptionText.Location = new System.Drawing.Point(12, 93);
             this.txtOptionText.Name = "txtOptionText";
             this.txtOptionText.Size = new System.Drawing.Size(198, 20);
             this.txtOptionText.TabIndex = 7;
@@ -110,7 +112,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 116);
+            this.label2.Location = new System.Drawing.Point(12, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(146, 13);
             this.label2.TabIndex = 8;
@@ -120,7 +122,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(216, 135);
+            this.label4.Location = new System.Drawing.Point(216, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(16, 16);
             this.label4.TabIndex = 11;
@@ -129,7 +131,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(238, 135);
+            this.label5.Location = new System.Drawing.Point(238, 96);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(90, 13);
             this.label5.TabIndex = 12;
@@ -139,7 +141,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(334, 135);
+            this.label6.Location = new System.Drawing.Point(334, 93);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(16, 16);
             this.label6.TabIndex = 13;
@@ -152,9 +154,9 @@
             this.comboFileExtension.Items.AddRange(new object[] {
             "CSV",
             "TXT"});
-            this.comboFileExtension.Location = new System.Drawing.Point(356, 132);
+            this.comboFileExtension.Location = new System.Drawing.Point(356, 93);
             this.comboFileExtension.Name = "comboFileExtension";
-            this.comboFileExtension.Size = new System.Drawing.Size(95, 21);
+            this.comboFileExtension.Size = new System.Drawing.Size(109, 21);
             this.comboFileExtension.TabIndex = 14;
             this.comboFileExtension.SelectedIndexChanged += new System.EventHandler(this.comboFileExtension_SelectedIndexChanged);
             // 
@@ -162,7 +164,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(12, 19);
+            this.label7.Location = new System.Drawing.Point(6, 9);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(125, 20);
             this.label7.TabIndex = 15;
@@ -171,7 +173,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(310, 258);
+            this.label8.Location = new System.Drawing.Point(310, 9);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(155, 13);
             this.label8.TabIndex = 16;
@@ -180,19 +182,39 @@
             // btnSplit
             // 
             this.btnSplit.Enabled = false;
-            this.btnSplit.Location = new System.Drawing.Point(297, 198);
+            this.btnSplit.Location = new System.Drawing.Point(346, 237);
             this.btnSplit.Name = "btnSplit";
-            this.btnSplit.Size = new System.Drawing.Size(154, 40);
+            this.btnSplit.Size = new System.Drawing.Size(119, 31);
             this.btnSplit.TabIndex = 17;
             this.btnSplit.Text = "Start Splitting";
             this.btnSplit.UseVisualStyleBackColor = true;
             this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ReportProgress);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lblPleaseWait
+            // 
+            this.lblPleaseWait.AutoSize = true;
+            this.lblPleaseWait.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPleaseWait.Location = new System.Drawing.Point(93, 87);
+            this.lblPleaseWait.Name = "lblPleaseWait";
+            this.lblPleaseWait.Size = new System.Drawing.Size(309, 50);
+            this.lblPleaseWait.TabIndex = 18;
+            this.lblPleaseWait.Text = "... Please Wait ...\r\nThis may take several minutes!";
+            this.lblPleaseWait.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblPleaseWait.Visible = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(477, 280);
+            this.Controls.Add(this.lblPleaseWait);
             this.Controls.Add(this.btnSplit);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -235,6 +257,8 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button btnSplit;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblPleaseWait;
 
     }
 }
--- GlemnetCSVSplitter/Form1.cs
+++ GlemnetCSVSplitter/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ using System.Windows.Forms;
 
 namespace CSVSplitter
 {
+    public struct ThreadNode
+    {
+        public string InputFileName;
+        public string InputFilePath;
+        public string OutputFilePath;
+        public string OutputFileName;
+        public string OutputFileExtension;
+        public int MaxRowsPerFile;
+    }
+
     public partial class frmMain : Form
     {
         OpenFileDialog ofd;
@@ -61,19 +72,153 @@ namespace CSVSplitter
 
         private void btnSplit_Click(object sender, EventArgs e)
         {
-            if (fileSplitter.SplitFile(ofd.FileName.Substring(0, (ofd.FileName.Length - ofd.SafeFileName.Length)), ofd.FileName, (int)splitAfter.Value, txtOptionText.Text + "_", comboFileExtension.Text))
+            ThreadNode data = new ThreadNode()
             {
-                MessageBox.Show("Your file was successfully split into multiple files!");
-            }
-            else
-            {
-                MessageBox.Show("Unfortunately, there was an error splitting your files!");
-            }
+                InputFileName = txtFileName.Text,
+                InputFilePath = ofd.FileName.Substring(0, (ofd.FileName.Length - ofd.SafeFileName.Length)),
+                MaxRowsPerFile = (int)splitAfter.Value,
+                OutputFileExtension = comboFileExtension.Text,
+                OutputFileName = txtOptionText.Text,
+                OutputFilePath = ofd.FileName.Substring(0, (ofd.FileName.Length - ofd.SafeFileName.Length))
+            };
+            startJob();
+            backgroundWorker1.RunWorkerAsync(data);
         }
 
         private void txtOptionText_TextChanged(object sender, EventArgs e)
         {
             CheckIfReady();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ThreadNode data = (ThreadNode)e.Argument;
+
+            if (string.IsNullOrEmpty(data.InputFileName))
+            {
+                // Cannot process the file, the filename is invalid
+                return;
+            }
+
+            // Create the new streamreader to access the file
+            StreamReader file = new StreamReader(data.InputFileName);
+
+            // Create a line string object
+            string line = string.Empty;
+
+            // Grab the header first for re-use
+            string header = file.ReadLine();
+
+            // Create the counting object
+            int currentRowCount = 1;
+
+            // Create the filename appendix count
+            int currentFileNumber = 0;
+
+            // Create the output string array
+            List<string> outputStrings = new List<string>();
+
+            // Loop through the file until there are no more lines to read
+            while ((line = file.ReadLine()) != null)
+            {
+                #region Save Operation
+
+                if (currentRowCount >= splitAfter.Value)
+                {
+                    SaveCurrentOutput(outputStrings, header, data.OutputFilePath, currentFileNumber, data.OutputFileName, data.OutputFileExtension);
+
+                    // Increase the file number count
+                    currentFileNumber++;
+
+                    // Reset the row counter
+                    currentRowCount = 1;
+
+                    // Clear the output strings list
+                    outputStrings.Clear();
+                }
+
+                #endregion Save Operation
+
+                else
+                {
+                    // We have not reached the maximum number of rows yet, so add this to the output strings
+                    outputStrings.Add(line);
+                    currentRowCount++;
+                }
+            }
+            SaveCurrentOutput(outputStrings, header, data.OutputFilePath, currentFileNumber, data.OutputFileName, data.OutputFileExtension);
+        }
+
+        private void SaveCurrentOutput(List<string> outputStrings, string header, string path, int currentFileNumber, string optionalText, string outputExtension)
+        {
+            string fileExtension = "." + outputExtension;
+
+            // We have reached the maximum number of rows for this file, save it out
+            using (StreamWriter newOutputFile = new StreamWriter(String.Format("{0}{1}{2}{3}", path, optionalText, currentFileNumber, fileExtension)))
+            {
+                // Preppend the header!
+                newOutputFile.WriteLine(header);
+                // Iterate through all output strings
+                foreach (string newLine in outputStrings)
+                {
+                    // And output them to the new file
+                    newOutputFile.WriteLine(newLine);
+                }
+                // Close the stream to save the file
+                newOutputFile.Close();
+            }
+#if DEBUG
+            // If debug mode, output to console
+            Console.WriteLine("Split new file: " + String.Format("{0}{1}{2}{3}", path, optionalText, currentFileNumber, fileExtension));
+#endif
+        }
+
+        private void startJob()
+        {
+            this.txtFileName.Visible = false;
+            this.txtOptionText.Visible = false;
+            this.btnSelectFile.Visible = false;
+            this.btnSplit.Visible = false;
+            this.comboFileExtension.Visible = false;
+            this.lblSplitAfter.Visible = false;
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+            this.label7.Visible = false;
+            this.label8.Visible = false;
+            this.splitAfter.Visible = false;
+            this.lblPleaseWait.Visible = true;
+        }
+
+        private void finishJob()
+        {
+            this.txtFileName.Visible = true;
+            this.txtOptionText.Visible = true;
+            this.btnSelectFile.Visible = true;
+            this.btnSplit.Visible = true;
+            this.comboFileExtension.Visible = true;
+            this.lblSplitAfter.Visible = true;
+            this.label1.Visible = true;
+            this.label2.Visible = true;
+            this.label4.Visible = true;
+            this.label5.Visible = true;
+            this.label6.Visible = true;
+            this.label7.Visible = true;
+            this.label8.Visible = true;
+            this.splitAfter.Visible = true;
+            this.lblPleaseWait.Visible = false;
+        }
+
+        private void backgroundWorker1_ReportProgress(object sender, ProgressChangedEventArgs e)
+        {
+            //lblPleaseWait.Text = e.ProgressPercentage.ToString();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            finishJob();
         }
     }
 }
